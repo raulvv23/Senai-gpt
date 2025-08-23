9f4859266f9d01fd5ac38e315636ca7daeebb797
raulvv23 <raulsantoss540@gmail.com>
2025-08-22 21:49:51 -0300

    Atividades JavaScript

---
+++ javascript/condicionais.js
@@ -0,0 +1,11 @@
+// Exercicio 1
+
+let idade = prompt("qual e a idade");
+
+if (idade >= 18) {
+    alert("parabens, voce e maior de idade");
+
+} else {
+    alert("vai pra super nani ");
+}
+
---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+console.log("Hello World!");
+
+// let nome = prompt("Qual o seu nome?");
+
+// let idade = prompt("Qual e sua idade?");
+
+// alert("meu nome e " + nome+ " tenho " + idade ); 
+
+let idade = prompt("qual e a idade");
+
+if (idade >= 18) {
+    alert("parabens, voce e maior de idade");
+
+} else {
+    alert("vai pra super nani ");
+}
+
+
+
