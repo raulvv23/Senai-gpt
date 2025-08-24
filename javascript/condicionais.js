7833282e1bf09bcac2bdd73caef7acd9e46d9b62
Raul Viana Dos Santos <raulsantoss540@gmail.com>
2025-08-24 12:19:54 -0300

    exercicios em andamento

--- javascript/condicionais.js
+++ javascript/condicionais.js
@@ -1,4 +1,4 @@
-// Exercicio 1
+//Exercicio 1
 
 let idade = prompt("qual e a idade");
 
@@ -8,4 +8,45 @@ if (idade >= 18) {
 } else {
     alert("vai pra super nani ");
 }
+
+
+//Exercicio 2
+
+let numero = Number(prompt("digite um numero"));
+
+if (numero > 0 ) {
+    console.log("O numero é positivo!");
+    
+} else {
+    console.log("O numero é negativo!");
+}
+
+
+//Exercicio 3
+
+let nota = Number(prompt("Digite a nota do aluno"));
+
+if (nota >= 60) {
+    alert (" Aluno aprovado!");
+
+} else{
+    alert ("Aluno reprovado!")
+}
+
+
+//Exercicio 4 
+
+let number = Number(prompt("Digite um numero:"));
+
+if (number > 0) {
+    alert("O numero é positivo");
+    
+} else if (number < 0) {
+    alert("O numero é negativo");
+
+} else {
+    alert("Seu numero é zero!");
+}
+
+
 
--- javascript/script.js
+++ javascript/script.js
@@ -4,16 +4,6 @@ console.log("Hello World!");
 
 // let idade = prompt("Qual e sua idade?");
 
-// alert("meu nome e " + nome+ " tenho " + idade ); 
-
-let idade = prompt("qual e a idade");
-
-if (idade >= 18) {
-    alert("parabens, voce e maior de idade");
-
-} else {
-    alert("vai pra super nani ");
-}
-
+// alert("meu nome e " + nome+ " tenho " + idade );
 
 
