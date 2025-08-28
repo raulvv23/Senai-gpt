5b7b0b7f212897c1463afde36f701f59413aecbf
raulvv23 <raulsantoss540@gmail.com>
2025-08-27 21:52:52 -0300

    Exercicios JS iniciados

---
+++ javascript/repeticoes.js
@@ -0,0 +1,42 @@
+// //Exercicio 1
+// console.log("FOR")
+// for(let contadora = 1; contadora <= 10; contadora++) {
+
+//     console.log(contadora);
+// }
+
+// console.log("WHILE")
+// let contadoraWhite = 1
+// while (contadoraWhite <= 5) {
+
+//     console.log(contadoraWhite);
+
+//     contadoraWhite++;
+     
+// }
+
+//exercicio 2 
+
+// let numero = prompt("digite um numero:")
+// let resultado = 0
+
+// for(let i = 1; i <=10; i++ ) {
+
+//     resultado = (numero * i )
+//     console.log("Seu resultado e " + resultado)
+
+
+// }
+
+
+//exercicio 3
+
+
+let number = prompt("Digite um numero:")
+soma=0
+
+for(let j = 1; j = number; j++) {
+
+    soma = (number + j)
+    console.log("Seu resultado e " + soma)
+}
